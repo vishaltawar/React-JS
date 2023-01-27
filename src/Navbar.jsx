47197d52dc8a9cb2d55a56f2942c1216ab850f71
vishaltawar <vishalmtawar0205@gmail.com>
2023-01-28 00:06:49 +0530

    Movie Card ,component Base Mini project

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+    return  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand">Movies</a>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </nav> 
+        
+}
+export default Navbar;
