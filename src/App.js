b156db62aec6ea533fc12b1593deeef9ed42d355
vishaltawar <vishalmtawar0205@gmail.com>
2023-01-28 00:08:52 +0530

    added some extra part

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import './App.css';
 // import Home from './Home';
 // import Bhojan from './Bhojan';
 // import Mony from './Mony'
-import { Add, Sub, Mul, Div } from './Calculator'
+// import { Add, Sub, Mul, Div } from './Calculator'
+import Cards from './MovieCards'
+
 
 function App() {
-  return (
-
+  return <Cards/>
+}
+export default App;
     /*    <div style={{ margin: '150px auto', width: '600px', backgroundColor: 'yellowgreen', padding: '15px', borderRadius: '5px ' }}>
           <Home />
           <Bhojan />
           <Mony />
         </div>
     */
-    <div style={{ margin: '100px 50px' }}>
+    /*<div style={{ margin: '100px 50px' }}>
       <h3>Small Calculator :- </h3>
       <ul style={{ margin: '20px 50px' }} >
         <li>Additon of 2 No. is {Add(5, 5)}</li>
@@ -21,9 +24,5 @@ function App() {
         <li>Multiplication of 2 No. is {Mul(2, 5)}</li>
         <li>Division of  2 No. is {Div(19, 3)}</li>
       </ul>
-    </div>
-  );
+    </div>*/
 
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-//var React = require("react");
-//var ReactDom = require("react-dom");
 import React from 'react';
 import ReactDom from 'react-dom';
-import "./index.css"
+import "./index.css";
+// import App from './App';
+import MovieCards from './MovieCards';
+import Navbar from './Navbar';
 
-import App from './App'
 
 
 
@@ -119,9 +119,48 @@ ReactDom.render(
 // ***********************************************************************//
 // task 5:-Create Component's use in App.js and 'render' here.
 
-ReactDom.render(<App />, document.getElementById('root'));
+// ReactDom.render(<App />, document.getElementById('root'));
 
 // ***********************************************************************//
 // Chalange task 6:-Create Calculator using import  & export& Component's.
 
-ReactDom.render(<App />,document.getElementById("root"));
+
+
+
+
+ReactDom.render(
+    <>
+    <Navbar/>
+    <div className='prop_card'>
+        <MovieCards
+         img="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/old_images/vertical/MOVIE/3314/1770003314/1770003314-v"
+            title="M.S Dhoni"
+            info="The Untold Story"
+            link="https://www.hotstar.com/in/movies/ms-dhoni-the-untold-story/1770003314"
+        />
+        <MovieCards
+         img="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/5457/1455457-v-64cc7681c13d"
+            title="Jhanda"
+            info="Uncha Rahe Humara: the Tricolor"
+            link="https://www.hotstar.com/in/movies/jhanda-uncha-rahe-humara-the-tricolor/1260129629"
+        />
+        <MovieCards
+         img="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7676/647676-v"
+            title="Tanaji"
+            info="The Unsung Warrior"
+            link="https://www.hotstar.com/in/movies/tanhaji/1260022117"
+        />
+        <MovieCards
+         img="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/old_images/vertical/MOVIE/8437/1000058437/1000058437-v"
+            title="Parayan"
+            link="https://www.hotstar.com/in/movies/payanam/1000058437"
+        />
+        <MovieCards
+         img="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/5457/1455457-v-64cc7681c13d"
+            title="Jhanda"
+            info="Uncha Rahe Humara: the Tricolor"
+            link="https://www.hotstar.com/in/movies/jhanda-uncha-rahe-humara-the-tricolor/1260129629"
+        />
+        </div>
+    </>
+    , document.getElementById("root"));
