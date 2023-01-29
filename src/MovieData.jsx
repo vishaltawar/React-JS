483a4afbb5143da546c5c06ca8ff36f32ab54074
vishaltawar <vishalmtawar0205@gmail.com>
2023-01-29 18:31:21 +0530

    Movie Data added

---
+++ src/MovieData.jsx
@@ -0,0 +1,38 @@
+const movieData = [
+
+    {
+        id: 1,
+        img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/old_images/vertical/MOVIE/3314/1770003314/1770003314-v",
+        title: "M.S Dhoni",
+        info: "The Untold Story",
+        link: "https://www.hotstar.com/in/movies/ms-dhoni-the-untold-story/1770003314"
+    },
+    {
+        id: 2,
+        img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/5457/1455457-v-64cc7681c13d",
+        title: "Jhanda",
+        info: "Uncha Rahe Humara: the Tricolor",
+        link: "https://www.hotstar.com/in/movies/jhanda-uncha-rahe-humara-the-tricolor/1260129629"
+    },
+    {
+        id: 3,
+        img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7676/647676-v",
+        title: "Tanaji",
+        info: "The Unsung Warrior",
+        link: "https://www.hotstar.com/in/movies/tanhaji/1260022117"
+    },
+    {
+        id: 4,
+        img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/old_images/vertical/MOVIE/8437/1000058437/1000058437-v",
+        title: "Parayan",
+        link: "https://www.hotstar.com/in/movies/payanam/1000058437"
+    },
+    {
+        id: 5,
+        img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/5219/875219-v",
+        title: "Avengers",
+        info: "Marvel Studios",
+        link: "https://www.hotstar.com/in/movies/marvels-the-avengers/1660000015"
+    }
+]
+export default movieData;
